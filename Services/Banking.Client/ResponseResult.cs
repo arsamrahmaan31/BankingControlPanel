ddb59653fe812cb72ad16cab5a8d65c826e74524
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 22:59:38 +0500

    Added generic response class for handling the response data

---
+++ Services/Banking.Client/ResponseResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Banking.Client
+{
+    public class ResponseResult<T>
+    {
+        public bool success { get; set; }
+        public T? result { get; set; }
+        public string? message { get; set; }
+        public string? token { get; set; }
+    }
+}
