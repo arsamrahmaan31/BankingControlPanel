87a950a3053753d08b6a02a2399f22f6cc5b512b
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 18:27:21 +0500

    Added HttpContext in the DI container, along with some comments in the Program file

--- Services/Banking.Auth/Program.cs
+++ Services/Banking.Auth/Program.cs
@@ -1,21 +1,31 @@
+using Banking.Auth.Logger;
+using Banking.Auth.Managers;
+using Banking.Auth.Repositories;
 using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+
+// Determine the configuration file based on the environment
 string routefile = string.Format("appsettings.{0}.json", builder.Environment.EnvironmentName);
+
+// Set configuration sources: JSON file and environment variables
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile(routefile, optional: false, reloadOnChange: true).AddEnvironmentVariables();
 
-
+// Configure Serilog using settings from the configuration file
 builder.Host.UseSerilog((context, configuration) =>
 configuration.ReadFrom.Configuration(context.Configuration));
 
+// Registering Managers and Repositories with dependency injection (DI) as a transient service
+builder.Services.AddTransient<IAuthManager, AuthManager>();
+builder.Services.AddTransient<IAuthRepository, AuthRepository>();
+builder.Services.AddTransient<IAuthLogger, AuthLogger>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
