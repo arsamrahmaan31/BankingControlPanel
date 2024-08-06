b825274a8394f64bf81d7c68ba15df0bedd2ff7c
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 23:40:04 +0500

    Created ClientRequest model for getting client data

---
+++ Services/Banking.Client/Models/AddClientRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Banking.Client.Models
+{
+    public class AddClientRequest
+    {
+        public int gender_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string personal_id { get; set; }
+        public string profile_photo { get; set; }
+        public string mobile_number { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public string zip_code { get; set; }
+    }
+}
