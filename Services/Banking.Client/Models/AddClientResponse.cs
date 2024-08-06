09f41d8ff479eff3d5475211f4c01c1603d2800d
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-07 01:13:36 +0500

    New model added for AddClient Response

---
+++ Services/Banking.Client/Models/AddClientResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Banking.Client.Models
+{
+    public class AddClientResponse
+    {
+        public int gender_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string personal_id { get; set; }
+        public string profile_photo { get; set; }
+        public string mobile_number { get; set; }
+
+        public List<ClientAccount> client_accounts { get; set; }
+        public ClientAddress address { get; set; }
+    }
+}
