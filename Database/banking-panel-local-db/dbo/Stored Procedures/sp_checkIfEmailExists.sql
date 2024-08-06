9f64d4a1a5f3204e934e4de5c21842c38dbc6035
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 19:37:58 +0500

    Add stored procedures in database project for user creation and email existence check

---
+++ Database/banking-panel-local-db/dbo/Stored Procedures/sp_checkIfEmailExists.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE PROCEDURE [dbo].[sp_checkIfEmailExists]
+    @email NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        u.user_id,
+        u.first_name,
+        u.last_name,
+        u.email,
+        u.password,
+        r.role_name,
+        u.role_id
+    FROM [bp_users] AS u
+    INNER JOIN [bp_lookup_roles] AS r 
+        ON u.role_id = r.role_id
+    WHERE u.email = @email;
+END
---
+++ Database/banking-panel-local-db/dbo/Stored Procedures/sp_createNewUser.sql
@@ -0,0 +1,26 @@
+﻿
+CREATE PROCEDURE [dbo].[sp_createNewUser]
+    @role_id INT,
+    @first_name NVARCHAR(255),
+    @last_name NVARCHAR(255),
+    @email NVARCHAR(50),
+    @password NVARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Insert a new user into the [bp_users] table
+    INSERT INTO [bp_users] (role_id, first_name, last_name, email, [password])
+    VALUES (@role_id, @first_name, @last_name, @email, @password);
+
+    -- Retrieve the details of the newly created user
+    SELECT 
+        r.role_name,
+        u.first_name,
+        u.last_name,
+        u.email
+    FROM [bp_users] AS u
+    INNER JOIN [bp_lookup_roles] AS r 
+        ON r.role_id = u.role_id
+    WHERE u.email = @email;
+END
