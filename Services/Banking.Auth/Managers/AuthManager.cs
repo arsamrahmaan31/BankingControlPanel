761ff38cb5c3a91f4471fa8b30fee9777c2f3ac3
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 18:44:58 +0500

    Added logic in the Auth Manager class in the Banking.Auth service

---
+++ Services/Banking.Auth/Managers/AuthManager.cs
@@ -0,0 +1,51 @@
+﻿using Banking.Auth.Constants;
+using Banking.Auth.Models;
+using Banking.Auth.Repositories;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Banking.Auth.Managers
+{
+    public class AuthManager(IAuthRepository authRepository, IConfiguration configuration): IAuthManager
+    {
+        public async Task<ResponseResult<LoginResponse>> LoginAsync(LoginRequest login)
+        {
+            try
+            {
+                var credentialsFound = await authRepository.IsLoginExistsAsync(login.email);
+                if (credentialsFound != null)
+                {
+                    if (BCrypt.Net.BCrypt.Verify(login.password, credentialsFound.password))
+                    {
+                        string token = string.Empty;
+                        
+                        var loggedInUser = new LoginResponse
+                        {
+                            first_name = credentialsFound.first_name,
+                            last_name = credentialsFound.last_name,
+                            email = credentialsFound.email,
+                            role_name = credentialsFound.role_name,
+                        };
+                        return new ResponseResult<LoginResponse> { success = true, result = loggedInUser, message = StaticMessages.TokenCreated, token = token };
+                    }
+                    else
+                    {
+                        return new ResponseResult<LoginResponse> { success = false, result = new LoginResponse(), message = StaticMessages.IncorrectPassword };
+                    }
+                }
+                else return new ResponseResult<LoginResponse> { success = false, result = new LoginResponse(), message = StaticMessages.UserNotExist };
+            }
+            catch (Exception ex)
+            {
+                Log.Error(StaticMessages.ExceptionOccured, nameof(LoginAsync), ex.Message);
+                return new ResponseResult<LoginResponse> { success = false, result = new LoginResponse(), message = ex.Message };
+            }
+        }
+
+
+
+    }
+}
---
+++ Services/Banking.Auth/Managers/IAuthManager.cs
@@ -0,0 +1,9 @@
+﻿using Banking.Auth.Models;
+
+namespace Banking.Auth.Managers
+{
+    public interface IAuthManager
+    {
+        Task<ResponseResult<LoginResponse>> LoginAsync(LoginRequest login);
+    }
+}
