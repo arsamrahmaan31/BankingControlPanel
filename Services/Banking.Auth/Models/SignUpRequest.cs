912de570072633b2b5778a5723cedfef9661e2f9
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 18:29:11 +0500

    Added SignUp Request and Response model in the Banking.Auth service

---
+++ Services/Banking.Auth/Models/SignUpRequest.cs
@@ -0,0 +1,19 @@
+﻿using Banking.Auth.HelperHandlers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Banking.Auth.Models
+{
+    public class SignUpRequest
+    {
+        [CompositeValidation]
+        public int role_id { get; set; }
+        [Required(ErrorMessage = "Frist Name is required.")]
+        public string first_name { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string last_name { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string password { get; set; }
+    }
+}
---
+++ Services/Banking.Auth/Models/UserVerificationResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Banking.Auth.Models
+{
+    public class UserVerificationResult
+    {
+        public int user_id { get; set; }
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+        public int? role_id { get; set; }
+        public string? role_name { get; set; }
+    }
+}
