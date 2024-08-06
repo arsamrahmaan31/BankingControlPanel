81bde02a2eb21e3d4717e7d70b816855365f73f8
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 19:29:04 +0500

    Added logic in the Auth Repository to check if the Email exist in the database for Banking.Auth service

---
+++ Services/Banking.Auth/Repositories/AuthRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using Banking.Auth.Models;
+using Dapper;
+using Serilog;
+using Banking.Auth.Constants;
+
+namespace Banking.Auth.Repositories
+{
+    public class AuthRepository(IConfiguration configuration): IAuthRepository
+    {
+
+        public async Task<UserVerificationResult> IsLoginExistsAsync(string email)
+        {
+            try
+            {
+                using var connection = CreateConnection();
+                await connection.OpenAsync();
+
+                // Passing email as a dynamic parameter for the stored procedure
+                var parameters = new { email };
+
+                // Execute the stored procedure
+                var user = await connection.QueryFirstOrDefaultAsync<UserVerificationResult>(
+                    SystemConstants.StoredProcedure_CheckIfEmailExists, parameters, commandType: CommandType.StoredProcedure
+                );
+
+                // Return the result
+                return user;
+            }
+            catch (SqlException sqlEx)
+            {
+                // Log the SQL-specific exception
+                Log.Error(StaticMessages.ExceptionOccured, nameof(SignUpAsync), sqlEx.Message);
+                throw sqlEx.InnerException;
+            }
+            catch (Exception ex)
+            {
+                // Log the application-specific exceptions
+                Log.Error(StaticMessages.ExceptionOccured, nameof(SignUpAsync), ex.Message);
+                throw ex;
+            }
+        }
+
+        
+
+
+        #region Private methods
+
+        private SqlConnection CreateConnection()
+        {
+            var connectionString = configuration.GetConnectionString(SystemConstants.DefaultConnection);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException(StaticMessages.DatabaseNotConfigured);
+            }
+            return new SqlConnection(connectionString);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Services/Banking.Auth/Repositories/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using Banking.Auth.Models;
+
+namespace Banking.Auth.Repositories
+{
+    public interface IAuthRepository
+    {
+        Task<UserVerificationResult> IsLoginExistsAsync(string email);
+    }
+}
