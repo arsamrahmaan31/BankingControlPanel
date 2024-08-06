cd8ce15f3d0ff993191d7675843136451077140c
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-06 15:47:24 +0500

    status codes added for banking.auth service

---
+++ Services/Banking.Auth/Constants/HttpStatusCodes.cs
@@ -0,0 +1,22 @@
+﻿namespace Banking.Auth.Constants
+{
+    public static class HttpStatusCodes
+    {
+        // Success codes
+        public const int OK = 200;
+        public const int Created = 201;
+
+        // Error codes
+        public const int BadRequest = 400;
+        public const int Unauthorized = 401;
+        public const int Forbidden = 403;
+        public const int NotFound = 404;
+        public const int MethodNotAllowed = 405;
+
+        // Server error codes
+        public const int InternalServerError = 500;
+        public const int NotImplemented = 501;
+        public const int BadGateway = 502;
+    }
+
+}
