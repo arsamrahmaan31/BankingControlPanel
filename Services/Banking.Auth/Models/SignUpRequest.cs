0e66eebeb06f205e5b4d757066ff0401b6d41e9c
arsamrahmaan31 <99415298+arsamrahmaan31@users.noreply.github.com>
2024-08-08 01:35:57 +0500

    Data Annotation added for invalid email

--- Services/Banking.Auth/Models/SignUpRequest.cs
+++ Services/Banking.Auth/Models/SignUpRequest.cs
@@ -12,6 +12,7 @@ namespace Banking.Auth.Models
         [Required(ErrorMessage = "Last Name is required.")]
         public string last_name { get; set; }
         [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string email { get; set; }
         [Required(ErrorMessage = "Password is required.")]
         public string password { get; set; }
